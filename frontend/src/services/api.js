5daa80b88774dd2d893e505643959ef3b57c10d6
princesoni6890 <sarrafprince37@gmail.com>
2026-06-28 15:18:31 +0530

    Updated API URL

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api/tasks",
+  baseURL: "https://task-tracker-mern-dayh.onrender.com/api/tasks",
 });
 
 export default API;
